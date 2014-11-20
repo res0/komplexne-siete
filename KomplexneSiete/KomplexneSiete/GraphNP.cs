d608a8948da3ef6d19aab059e06422444c96c011
kezo19 <kuzel.marek9@gmail.com>
2014-11-20 12:42:51 +0100

    zmena

--- KomplexneSiete/KomplexneSiete/GraphNP.cs
+++ KomplexneSiete/KomplexneSiete/GraphNP.cs
@@ -7,9 +7,12 @@ namespace KomplexneSiete
 {
     public class GraphNP : Graph
     {
-        public override void Generate()
+        public override void Generate(int n, double p)
         {
-
+            for (int i = 0; i < n; i++)
+            {
+                this.AddNode(new List<int>());
+            }
         }
     }
 }
